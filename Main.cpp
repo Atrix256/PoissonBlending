0e8aba641d8036f7d13419d388c40b0207459276
Atrix256 <alan.wolfe@gmail.com>
2018-02-08 20:34:48 -0800

    sRGB correct, clamp and work in float

--- Main.cpp
+++ Main.cpp
@@ -10,24 +10,38 @@
 #define STBI_MSC_SECURE_CRT
 #include "stb/stb_image_write.h"
 
+const float c_gamma = 2.2f;
+
 struct SImageInfo
 {
-    stbi_uc* m_pixels   = nullptr;
-    int m_width         = 0;
-    int m_height        = 0;
-    int m_channels      = 0;
+    std::vector<float>  m_pixels;
+    int m_width = 0;
+    int m_height = 0;
+    int m_channels = 0;
 
-    bool Load (const char *fileName)
+    bool Load(const char *fileName)
     {
-        m_pixels = stbi_load(fileName, &m_width, &m_height, &m_channels, 3);
-        if (m_pixels == nullptr)
+        // load image
+        stbi_uc* pixels = stbi_load(fileName, &m_width, &m_height, &m_channels, 3);
+        if (pixels == nullptr)
+        {
             printf("Could not load file %s\n", fileName);
-        return m_pixels != nullptr;
-    }
+            return false;
+        }
 
-    ~SImageInfo()
-    {
-        stbi_image_free(m_pixels);
+        // convert to float and convert from sRGB to linear
+        m_pixels.resize(m_width*m_height * 3);
+        stbi_uc* srcPixel = pixels;
+        for (float& pixel : m_pixels)
+        {
+            pixel = float(*srcPixel) / 255.0f;
+            pixel = std::powf(pixel, c_gamma);
+            ++srcPixel;
+        }
+
+        // free pixels and return success
+        stbi_image_free(pixels);
+        return true;
     }
 };
 
@@ -36,17 +50,31 @@ struct SRect
     int x1, y1, x2, y2;
 };
 
-bool WriteImage (const char *fileName, int width, int height, stbi_uc* pixels)
+bool WriteImage (const char *fileName, int width, int height, std::vector<float>& pixels)
 {
-    return stbi_write_png(fileName, width, height, 3, pixels, 3 * width) != 0;
+    // convert from linear to sRGB, clamp, and convert to uint8.
+    std::vector<stbi_uc> outPixels;
+    outPixels.resize(width*height * 3);
+    float* srcPixel = &pixels[0];
+    for (stbi_uc& pixel : outPixels)
+    {
+        float value = std::powf(*srcPixel, 1.0f / c_gamma);
+        if (value < 0.0f)
+            value = 0.0f;
+        else if (value > 1.0f)
+            value = 1.0f;
+        pixel = stbi_uc(value*255.0f);
+        ++srcPixel;
+    }
+
+    return stbi_write_png(fileName, width, height, 3, &outPixels[0], 3 * width) != 0;
 }
 
 void NaivePaste (const SImageInfo &source, const SImageInfo &dest, int pasteX, int pasteY, const char* fileName)
 {
     // copy the destination image to an out image buffer
-    std::vector<stbi_uc> outPixels;
-    outPixels.resize(dest.m_width*dest.m_height * 3);
-    memcpy(&outPixels[0], dest.m_pixels, outPixels.size());
+    std::vector<float> outPixels;
+    outPixels = dest.m_pixels;
 
     // calculate details of paste, handling negative paste locations and images larger than the destination etc.
     SRect sourceRect = { 0, 0, source.m_width, source.m_height };
@@ -85,23 +113,22 @@ void NaivePaste (const SImageInfo &source, const SImageInfo &dest, int pasteX, i
     {
         for (int y = 0; y < copyHeight; ++y)
         {
-            stbi_uc* sourcePixels = &source.m_pixels[((sourceRect.y1 + y)*source.m_width + sourceRect.x1) * 3];
-            stbi_uc* destPixels = &outPixels[((destRect.y1 + y)*dest.m_width + destRect.x1) * 3];
-            memcpy(destPixels, sourcePixels, copyWidth * 3);
+            const float* sourcePixels = &source.m_pixels[((sourceRect.y1 + y)*source.m_width + sourceRect.x1) * 3];
+            float* destPixels = &outPixels[((destRect.y1 + y)*dest.m_width + destRect.x1) * 3];
+            memcpy(destPixels, sourcePixels, copyWidth * 3 * sizeof(sourcePixels[0]));
         }
     }
 
     // write the file
-    if (!WriteImage(fileName, dest.m_width, dest.m_height, &outPixels[0]))
+    if (!WriteImage(fileName, dest.m_width, dest.m_height, outPixels))
         printf(__FUNCTION__ "() error: Could not write %s\n", fileName);
 }
 
-void NaiveGradientPaste (const SImageInfo &source, std::vector<int>& sourceGradient, const SImageInfo &dest, int pasteX, int pasteY, const char* fileName)
+void NaiveGradientPaste (const SImageInfo &source, std::vector<float>& sourceGradient, const SImageInfo &dest, int pasteX, int pasteY, const char* fileName)
 {
     // copy the destination image to an out image buffer
-    std::vector<stbi_uc> outPixels;
-    outPixels.resize(dest.m_width*dest.m_height * 3);
-    memcpy(&outPixels[0], dest.m_pixels, outPixels.size());
+    std::vector<float> outPixels;
+    outPixels = dest.m_pixels;
 
     // calculate details of paste, handling negative paste locations and images larger than the destination etc.
     SRect sourceRect = { 0, 0, source.m_width, source.m_height };
@@ -140,9 +167,9 @@ void NaiveGradientPaste (const SImageInfo &source, std::vector<int>& sourceGradi
     {
         for (int y = 1; y < copyHeight; ++y)
         {
-            stbi_uc* sourcePixel = &source.m_pixels[((sourceRect.y1 + y)*source.m_width + sourceRect.x1) * 3];
-            int* sourceGradientPixel = &sourceGradient[((sourceRect.y1 + y)*source.m_width + sourceRect.x1) * 6];
-            stbi_uc* destPixel = &outPixels[((destRect.y1 + y)*dest.m_width + destRect.x1) * 3];
+            const float* sourcePixel = &source.m_pixels[((sourceRect.y1 + y)*source.m_width + sourceRect.x1) * 3];
+            const float* sourceGradientPixel = &sourceGradient[((sourceRect.y1 + y)*source.m_width + sourceRect.x1) * 6];
+            float* destPixel = &outPixels[((destRect.y1 + y)*dest.m_width + destRect.x1) * 3];
             for (int x = 1; x < copyWidth; ++x)
             {
                 destPixel[0] = destPixel[-3] + sourceGradientPixel[0];
@@ -158,34 +185,34 @@ void NaiveGradientPaste (const SImageInfo &source, std::vector<int>& sourceGradi
     }
 
     // write the file
-    if (!WriteImage(fileName, dest.m_width, dest.m_height, &outPixels[0]))
+    if (!WriteImage(fileName, dest.m_width, dest.m_height, outPixels))
         printf(__FUNCTION__ "() error: Could not write %s\n", fileName);
 }
 
-void MakeImageGradient(const SImageInfo& source, std::vector<int>& sourceGradient)
+void MakeImageGradient(const SImageInfo& source, std::vector<float>& sourceGradient)
 {
     // allocate space for the gradients
     sourceGradient.resize(source.m_width*source.m_height * 6);
-    std::fill(sourceGradient.begin(), sourceGradient.end(), 0);
+    std::fill(sourceGradient.begin(), sourceGradient.end(), 0.0f);
 
     // make the gradients!
-    const stbi_uc* sourcePixel = source.m_pixels;
-    const stbi_uc* sourcePixelNextRow = sourcePixel + source.m_width * 3;
+    const float* sourcePixel = &source.m_pixels[0];
+    const float* sourcePixelNextRow = sourcePixel + source.m_width * 3;
 
-    int* destPixel = &sourceGradient[0];
+    float* destPixel = &sourceGradient[0];
     for (int y = 0; y < source.m_height-1; ++y)
     {
         for (int x = 0; x < source.m_width-1; ++x)
         {
             // calculate RGB dfdx
-            destPixel[0] = (int)sourcePixel[3] - (int)sourcePixel[0];
-            destPixel[1] = (int)sourcePixel[4] - (int)sourcePixel[1];
-            destPixel[2] = (int)sourcePixel[5] - (int)sourcePixel[2];
+            destPixel[0] = sourcePixel[3] - sourcePixel[0];
+            destPixel[1] = sourcePixel[4] - sourcePixel[1];
+            destPixel[2] = sourcePixel[5] - sourcePixel[2];
 
             // calculate RGB dfdy
-            destPixel[3] = (int)sourcePixelNextRow[0] - (int)sourcePixel[0];
-            destPixel[4] = (int)sourcePixelNextRow[1] - (int)sourcePixel[1];
-            destPixel[5] = (int)sourcePixelNextRow[2] - (int)sourcePixel[2];
+            destPixel[3] = sourcePixelNextRow[0] - sourcePixel[0];
+            destPixel[4] = sourcePixelNextRow[1] - sourcePixel[1];
+            destPixel[5] = sourcePixelNextRow[2] - sourcePixel[2];
 
             // move to the next pixels
             sourcePixel += 3;
@@ -195,34 +222,34 @@ void MakeImageGradient(const SImageInfo& source, std::vector<int>& sourceGradien
     }
 }
 
-void SaveImageGradient(const SImageInfo& source, const std::vector<int>& sourceGradient, const char* fileName)
+void SaveImageGradient(const SImageInfo& source, const std::vector<float>& sourceGradient, const char* fileName)
 {
     // Save the gradient as a side by side double wide image.
     // The left side is the x axis partial derivatives, the right side is the y axis.
-    std::vector<stbi_uc> outPixels;
+    std::vector<float> outPixels;
     outPixels.resize((source.m_width * 3 * source.m_height) * 3);
 
-    const stbi_uc* sourcePixel0 = source.m_pixels;
-    const int* sourcePixel1 = &sourceGradient[0];
+    const float* sourcePixel0 = &source.m_pixels[0];
+    const float* sourcePixel1 = &sourceGradient[0];
 
     for (int y = 0; y < source.m_height; ++y)
     {
-        stbi_uc* destPixel0 = &outPixels[y*source.m_width * 9];
-        stbi_uc* destPixel1 = &outPixels[y*source.m_width * 9 + source.m_width * 3];
-        stbi_uc* destPixel2 = &outPixels[y*source.m_width * 9 + source.m_width * 6];
+        float* destPixel0 = &outPixels[y*source.m_width * 9];
+        float* destPixel1 = &outPixels[y*source.m_width * 9 + source.m_width * 3];
+        float* destPixel2 = &outPixels[y*source.m_width * 9 + source.m_width * 6];
         for (int x = 0; x < source.m_width; ++x)
         {
             destPixel0[0] = sourcePixel0[0];
             destPixel0[1] = sourcePixel0[1];
             destPixel0[2] = sourcePixel0[2];
 
-            destPixel1[0] = stbi_uc((sourcePixel1[0] + 255) / 2);
-            destPixel1[1] = stbi_uc((sourcePixel1[1] + 255) / 2);
-            destPixel1[2] = stbi_uc((sourcePixel1[2] + 255) / 2);
+            destPixel1[0] = (sourcePixel1[0] * 0.5f) + 0.5f;
+            destPixel1[1] = (sourcePixel1[1] * 0.5f) + 0.5f;
+            destPixel1[2] = (sourcePixel1[2] * 0.5f) + 0.5f;
 
-            destPixel2[0] = stbi_uc((sourcePixel1[3] + 255) / 2);
-            destPixel2[1] = stbi_uc((sourcePixel1[4] + 255) / 2);
-            destPixel2[2] = stbi_uc((sourcePixel1[5] + 255) / 2);
+            destPixel2[0] = (sourcePixel1[3] * 0.5f) + 0.5f;
+            destPixel2[1] = (sourcePixel1[4] * 0.5f) + 0.5f;
+            destPixel2[2] = (sourcePixel1[5] * 0.5f) + 0.5f;
 
             // move to the next pixels
             destPixel0 += 3;
@@ -234,7 +261,7 @@ void SaveImageGradient(const SImageInfo& source, const std::vector<int>& sourceG
     }
 
     // save the file
-    if (!WriteImage(fileName, source.m_width*3, source.m_height, &outPixels[0]))
+    if (!WriteImage(fileName, source.m_width*3, source.m_height, outPixels))
         printf(__FUNCTION__ "() error: Could not write %s\n", fileName);
 }
 
@@ -267,7 +294,7 @@ int main(int argc, char** argv)
     NaivePaste(source, dest, pasteX, pasteY, "out_paste_naive.png");
 
     // make the source image gradient and save it
-    std::vector<int> sourceGradient;
+    std::vector<float> sourceGradient;
     MakeImageGradient(source, sourceGradient);
     SaveImageGradient(source, sourceGradient, "out_gradient.png");
 
@@ -283,7 +310,17 @@ int main(int argc, char** argv)
 
 TODO:
 
+? try alpha fading the edges of the image in naive paste to hide the discontinuity?
+
+* use alpha channel as mask.
+
+* work in linear space
+
+* clamp colors i guess
+
 * the partial derivatives add too much i think since they have a full sized delta on X and Y axis. Do i need to chop it in half or something maybe?
+ * i think the real issue is they are just constraints that need to be obeyed.
+
 * i think the mask really will be needed. the paste destroys the background!
 
 
